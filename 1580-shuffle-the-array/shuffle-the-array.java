0564f4d2ca6e3f383553a2681966186d09eadd1f
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-09-10 03:13:11 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (41.13%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] a = new int[2*n];
+        int k = 0;
+        for(int i=0;i<n;i++){
+            a[k++]=nums[i];
+            a[k++]=nums[i+n];
+        }
+        return a;
+    }
+}
